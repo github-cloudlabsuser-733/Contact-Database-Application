be36dd932984ba6c607c498521a764f1c30d8376
github_cloudlabsuser_733@cloudlabsaiuser.com <github_cloudlabsuser_733@cloudlabsaiuser.com>
2024-05-03 17:31:51 +0000

    The most significant changes in the code are the addition of new functionalities in the UserController.cs file, the addition of two new PropertyGroups for Debug|x64 and Release|x64 configurations in the CRUD application 2.csproj file, and the updates in the CRUD application 2.sln file.
    1. In the UserController.cs file, new functionalities were added to the Index, Details, Create, Edit, Delete, and Search methods in the UserController class. These changes enable the application to perform CRUD operations on User objects and search for users by name. This is a crucial update as it directly impacts the application's functionality and user interaction.
    
    2. The CRUD application 2.csproj file saw the addition of two new PropertyGroups for Debug|x64 and Release|x64 configurations. These configurations specify that the application should be built for the x64 platform in both Debug and Release modes. This change is important as it determines the build and execution environment of the application.
    
    3. The CRUD application 2.sln file was updated to change the Visual Studio version from 16 to 17, add a new project called UserControllerTest, and add Debug|x64 and Release|x64 configurations for both projects. This update is significant as it affects the development environment and testing of the application.
    
    Other changes include the addition and removal of a reference to the Microsoft.CodeDom.Providers.DotNetCompilerPlatform library in the CRUD application 2.csproj file, a minor change in the same file that does not affect functionality, updates to the Index.cshtml file, and a change in the Web.config file to update the bindingRedirect for the WebGrease assembly.
    
    The most important changes in the code are:
    
    1. The CRUD application 2.csproj file has been modified to add and then remove a reference to the Microsoft.CodeDom.Providers.DotNetCompilerPlatform library. This library is used for dynamic compilation of ASP.NET projects at runtime. The removal of this reference suggests a change in the project's dependencies or a decision to use a different compiler.
    
    2. The CRUD application 2.csproj file has also been updated to add two new PropertyGroups for the 'Debug|x64' and 'Release|x64' configurations. These configurations specify that the project should be built with debugging symbols, output to the 'bin\' directory, target the x64 platform, and use version 7.3 of the C# language.
    
    3. The CRUD application 2.sln file has been updated to increase the Visual Studio version from 16 to 17, add a new project called 'UserControllerTest', and add 'Debug|x64' and 'Release|x64' configurations for both projects.
    
    4. The UserController.cs file has been updated to add a 'using System;' directive, implement the Index, Details, Create, Edit, Delete, and Search methods of the UserController class, and add a static list of User objects to serve as the application's data store.
    
    5. The Index.cshtml file has been updated to modify the foreach loop to use the '+' operator instead of the '-' operator, and add a form for searching users by name.
    
    6. The Web.config file has been updated to change the bindingRedirect for the WebGrease assembly to redirect from any version to version '1.6.5135.21930'. This could be to ensure that the application uses a specific version of the WebGrease library.
    
    List of changes:
    
    1. CRUD application 2.csproj: Added and then removed reference to Microsoft.CodeDom.Providers.DotNetCompilerPlatform library.
    2. CRUD application 2.csproj: Added 'Debug|x64' and 'Release|x64' configurations.
    3. CRUD application 2.sln: Updated Visual Studio version, added 'UserControllerTest' project, and added 'Debug|x64' and 'Release|x64' configurations.
    4. UserController.cs: Added 'using System;' directive, implemented methods, and added static list of User objects.
    5. Index.cshtml: Updated foreach loop and added user search form.
    6. Web.config: Updated bindingRedirect for WebGrease assembly.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,66 +1,117 @@
 using CRUD_application_2.Models;
+using System;
 using System.Linq;
 using System.Web.Mvc;
- 
+
 namespace CRUD_application_2.Controllers
 {
     public class UserController : Controller
     {
         public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
-        // GET: User
-        public ActionResult Index()
+         // GET: User
+        public ActionResult Index(string searchString)
         {
-            // Implement the Index method here
+            var users = from u in userlist
+                        select u;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.Name.Contains(searchString));
+            }
+            return View("Index",users.ToList());
         }
- 
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            User user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Details",user);
         }
- 
+
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            // Create a new instance of the User class
+            User user = new User();
+
+            // Return the Create view with the user object
+            return View("Create",user);
         }
- 
+
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            // Add the user to the userlist
+            userlist.Add(user);
+
+            // Redirect to the Index action to display the updated list of users
+            return RedirectToAction("Index");
         }
- 
-        // GET: User/Edit/5
+
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            User user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
-        // POST: User/Edit/5
+
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            User existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
- 
-        // GET: User/Delete/5
+
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            User user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
-        // POST: User/Delete/5
+
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            User user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                userlist.Remove(user);
+            }
+            return RedirectToAction("Index");
         }
+
+
+        // GET: User/Search
+        public ActionResult Search(string searchString)
+        {
+            var users = from u in userlist
+                        select u;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.Name.Contains(searchString));
+            }
+            return View(users.ToList());
+        }
+
     }
 }
